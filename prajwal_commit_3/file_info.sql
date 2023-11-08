28833145e68f894f633395ee4dcc85106d7a5cdf
Prajwal Rawoorkar <78078356+Deadheaven@users.noreply.github.com>
2023-11-08 21:03:44 +0530

    commit_3

---
+++ prajwal_commit_3/file_info.sql
@@ -0,0 +1,9 @@
+create database file_info;
+use file_info;
+
+select * from file_name_info;
+select * from audit_info;
+select * from xls_file_sheet;
+select * from xls_file_sheet_row;
+select * from strato_zone;
+
